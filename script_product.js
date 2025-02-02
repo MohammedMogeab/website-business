55f912179121c639dd2141e67f4d43c2ff190c92
MubarakAshrafAlrawy <mubarakashrafalrawy@gmail.com>
2025-02-02 18:21:58 +0300

    add comments bar and video image show in product

--- script_product.js
+++ script_product.js
@@ -1,19 +1,42 @@
-const imgs = document.querySelectorAll('.img-select a');
-const imgBtns = [...imgs];
-let imgId = 1;
+// const imgs = document.querySelectorAll('.img-select a');
+// const imgBtns = [...imgs];
+// let imgId = 1;
 
-imgBtns.forEach((imgItem) => {
-    imgItem.addEventListener('click', (event) => {
-        event.preventDefault();
-        imgId = imgItem.dataset.id;
-        slideImage();
-    });
-});
+// imgBtns.forEach((imgItem) => {
+//     imgItem.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         imgId = imgItem.dataset.id;
+//         slideImage();
+//     });
+// });
 
-function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+
+//change image vidio
+const video = document.getElementById("mainVidio");
+const image = document.getElementById("mainImage");
+
+function selected_item(thumbnail){
+    const type = thumbnail.getAttribute("data-type");
+    if (type === 'video') {
+        // إظهار الفيديو وإخفاء الصورة
+         document.getElementById("vidiosource").src = thumbnail.getAttribute("vidsource");
+        video.style.display = 'block';
+        image.style.display = 'none'
+        image.style.transition = '0.5 ease';
+        
+    } else {
+        // إخفاء الفيديو وإظهار الصورة
+        video.style.display = 'none';
+        image.src = thumbnail.getAttribute('src');
+        image.style.display = 'block';
+        image.transition = '0.5 ease';
+    }
 }
+// function slideImage(){
+//     const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
-window.addEventListener('resize', slideImage);
+//     document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+// }
+
+// window.addEventListener('resize', slideImage);
